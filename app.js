35832fb9cd18a8bb2e59c25e3dc435f95a4921c5
Jason Vinson <vinsonizer@gmail.com>
2016-06-28 20:58:19 -0400

    Added socket.io support for votes

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var socket_io = require('socket.io');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var io = socket_io();
+app.io = io;
+
+io.sockets.on('connection', routes.vote);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +23,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Polls' });
+  res.render('index', {
+    title: 'Polls'
+  });
 });
 
 // JSON API for list of polls
-router.get('/polls/polls',  function(req, res) {
+router.get('/polls/polls', function(req, res) {
   Poll.find({}, 'question', function(error, polls) {
     res.json(polls);
   });
@@ -21,19 +23,24 @@ router.get('/polls/polls',  function(req, res) {
 // JSON API for getting a single poll
 router.get('/polls/:id', function(req, res) {
   var pollId = req.params.id;
-  Poll.findById(pollId, '', { lean: true }, function(err, poll) {
-    if(poll) {
+  Poll.findById(pollId, '', {
+    lean: true
+  }, function(err, poll) {
+    if (poll) {
       var userVoted = false,
-          userChoice,
-          totalVotes = 0;
-      for(var c in poll.choices) {
+        userChoice,
+        totalVotes = 0;
+      for (var c in poll.choices) {
         var choice = poll.choices[c];
-        for(var v in choice.votes) {
+        for (var v in choice.votes) {
           var vote = choice.votes[v];
           totalVotes++;
-          if(vote.ip === (req.header('x-forwarded-for') || req.ip)) {
+          if (vote.ip === (req.header('x-forwarded-for') || req.ip)) {
             userVoted = true;
-            userChoice = { _id: choice._id, text: choice.text };
+            userChoice = {
+              _id: choice._id,
+              text: choice.text
+            };
           }
         }
       }
@@ -42,7 +49,9 @@ router.get('/polls/:id', function(req, res) {
       poll.totalVotes = totalVotes;
       res.json(poll);
     } else {
-      res.json({error:true});
+      res.json({
+        error: true
+      });
     }
   });
 });
@@ -50,16 +59,60 @@ router.get('/polls/:id', function(req, res) {
 // JSON API for creating a new poll
 router.post('/polls', function(req, res) {
   var reqBody = req.body,
-      choices = reqBody.choices.filter(function(v) { return v.text !== ''; }),
-      pollObj = {question: reqBody.question, choices: choices};
+    choices = reqBody.choices.filter(function(v) {
+      return v.text !== '';
+    }),
+    pollObj = {
+      question: reqBody.question,
+      choices: choices
+    };
   var poll = new Poll(pollObj);
   poll.save(function(err, doc) {
-    if(err || !doc) {
+    if (err || !doc) {
       throw 'Error';
     } else {
       res.json(doc);
     }
   });
 });
+
+router.vote = function(socket) {
+  socket.on('send:vote', function(data) {
+    var ip = socket.handshake.headers['x-forwarded-for'] ||
+      socket.handshake.address.address;
+    Poll.findById(data.poll_id, function(err, poll) {
+      var choice = poll.choices.id(data.choice);
+      choice.votes.push({
+        ip: ip
+      });
+      poll.save(function(err, doc) {
+        var theDoc = {
+          question: doc.question,
+          _id: doc._id,
+          choices: doc.choices,
+          userVoted: false,
+          totalVotes: 0
+        };
+        for (var i = 0, ln = doc.choices.length; i < ln; i++) {
+          var choice = doc.choices[i];
+          for (var j = 0, jLn = choice.votes.length; j < jLn; j++) {
+            var vote = choice.votes[j];
+            theDoc.totalVotes++;
+            theDoc.ip = ip;
+            if (vote.ip === ip) {
+              theDoc.userVoted = true;
+              theDoc.userChoice = {
+                _id: choice._id,
+                text: choice.text
+              };
+            }
+          }
+        }
+        socket.emit('myvote', theDoc);
+        socket.broadcast.emit('vote', theDoc);
+      });
+    });
+  });
+};
 
 module.exports = router;
